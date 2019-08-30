38bc37ef8f9ca5119c8b2128c5329f9f7c0ca2ca
756D6D61 <756D6D61@gmail.com>
2019-08-30 12:48:31 +0100

    working api without a db.

--- server.js
+++ server.js
@@ -6,13 +6,61 @@ const bodyParser = require('body-parser'); //req.body
 const cors = require('cors'); //cross origin resource sharing 
 let contacts = require('./data');
 
-
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
 app.get('/api/contacts', (request, response) => {
+    if(!contacts) {
+        response.status(404).json({ message: 'No contacts found' });
+    }
     response.json(contacts);
+});
+
+app.get('/api/contacts/:id', (request, response) => {
+    const requestId = request.params.id;
+    let contact = contacts.filter(contact => {
+        return contact.id == requestId;
+    })
+    if(!contact) {
+        response.status(404).json({ message: 'No contact found' });
+    }
+    response.json(contact[0])
+})
+
+app.post('/api/contacts', (request, response) => {
+    const contact = {
+        id: contact.length + 1,
+        first_name: request.body.first_name,
+        last_name: request.body.last_name,
+        email: request.body.email,
+        website: request.body.wesbite
+    }
+    contacts.push(contact);
+    response.json(contact);
+})
+
+app.put('/api/contacts/:id', (request, response) => {
+    const requestId = request.params.id;
+    let contact = contacts.filter(contact => {
+        return contact.id == requestId;
+    })[0];
+    const index = contacts.indexOf(contact);
+    const keys = Object.keys(request.body);
+    keys.forEach(key => {
+        contact[key] = request.body[key];
+    });
+    contacts[index]= contact;
+    response.json(contacts[index])
+})
+
+app.delete('/api/contacts/:id', (request, resonse) => {
+    const requestId = request.params.id;
+    let contact = contacts.filter(contact => {
+        return contact.id == requestId;
+    })[0];
+    const index = contacts.indexOf(contact);
+    contacts.splice(index, 1);
+    response.json({ message: `User ${requestId} deleted.`})
 });
 
 const hostname = 'localhost';
