26e44c1fd9ebdf3b4c321a6d8d9ab95706212138
756D6D61 <756D6D61@gmail.com>
2019-08-30 12:19:52 +0100

    localhost:3001/api/contacts/ works.

--- server.js
+++ server.js
@@ -4,12 +4,15 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser'); //req.body
 const cors = require('cors'); //cross origin resource sharing 
+let contacts = require('./data');
+
+
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
 app.get('/api/contacts', (request, response) => {
-    response.send('Hello World');
+    response.json(contacts);
 });
 
 const hostname = 'localhost';
